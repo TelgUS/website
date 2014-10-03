880c9bab5057023de3a41f4f7c624f2c39079f96
Praveen Gattu <pgattu@gmail.com>
2014-10-02 21:28:23 -0700

    test

--- head.php
+++ head.php
@@ -4,24 +4,27 @@ define ( "MY_COMPANY", "TelgUS" );
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="telugu movies events india andhra pradesh">
+<meta name="description"
+	content="telugu movies events india andhra pradesh">
 <meta name="author" content="Praveen Gattu">
 
 <!-- Bootstrap -->
-<!--  the below stylesheet is the default theme for bootstrap -->
-<link href="/css/bootstrap.min.css" rel="stylesheet">
+<!--  the below stylesheet is the default theme for bootstrap 
+<link href="/css/bootstrap.min.css" rel="stylesheet"> -->
 <!--  the below stylesheet is the Flatly theme for bootstrap from bootswatch 
 <link href="http://bootswatch.com/flatly/bootstrap.min.css" rel="stylesheet"> -->
-<!--  the below stylesheet is the Amelia theme for bootstrap from bootswatch
+<!--  the below stylesheet is the Amelia theme for bootstrap from bootswatch 
 <link href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/amelia/bootstrap.min.css" rel="stylesheet"> -->
 <!--  the below stylesheet is the Cosmo theme for bootstrap from bootswatch 
 <link href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/cosmo/bootstrap.min.css" rel="stylesheet"> -->
 <!--  the below stylesheet is the Darkly theme for bootstrap from bootswatch
 <link href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/darkly/bootstrap.min.css" rel="stylesheet"> -->
-<!--  the below stylesheet is the Simplex theme for bootstrap from bootswatch
+<!--  the below stylesheet is the Simplex theme for bootstrap from bootswatch 
 <link href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/simplex/bootstrap.min.css" rel="stylesheet"> -->
-<!--  the below stylesheet is the Spacelab theme for bootstrap from bootswatch
-<link href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/spacelab/bootstrap.min.css" rel="stylesheet"> -->
+<!--  the below stylesheet is the Spacelab theme for bootstrap from bootswatch -->
+<link
+	href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/spacelab/bootstrap.min.css"
+	rel="stylesheet">
 
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -34,7 +37,5 @@ define ( "MY_COMPANY", "TelgUS" );
 <link href="/css/telgus.css" rel="Stylesheet">
 
 <!-- favicon -->
-<link rel="shortcut icon"
-	href="/images/favicon.ico" type="image/x-icon">
-<link rel="icon" href="/images/favicon.ico"
-	type="image/x-icon">
+<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/images/favicon.ico" type="image/x-icon">
