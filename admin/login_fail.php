0eb9e7055daf92c53818f0b6995d0dfeaa7cadc0
Praveen Gattu <pgattu@gmail.com>
2014-10-02 21:29:31 -0700

    checkin

---
+++ admin/login_fail.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php require_once "../head.php"; ?>
+	<title>Login</title>
+
+<!-- Custom styles for this template -->
+<link href="login.css" rel="stylesheet">
+</head>
+
+<body>
+	<div class="container">
+		<div class="bg-danger">
+			<strong>Invalid login! <a href="login.php">Try again</a></strong>
+		</div>
+
+	</div>
+
+	<?php require_once "admin_footer.php"; ?>
+</body>
+</html>
